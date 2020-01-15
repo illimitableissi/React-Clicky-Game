b5bd9c8ed3ec878178b15619305d97940065ab86
SOVEREIGN <isimemeni@gmail.com>
2020-01-14 21:58:45 -0500

    Updates

---
+++ precache-manifest.2d2498500a6d96cdf5944cc143b9a5b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199653fed61e797b16b2f9f2b7276f91",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "41d98dcf335bba6dad9c",
+    "url": "/React-Clicky-Game/static/css/main.7cc327b3.chunk.css"
+  },
+  {
+    "revision": "7cab17352a316dca7cc9",
+    "url": "/React-Clicky-Game/static/js/2.f13001f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Clicky-Game/static/js/2.f13001f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41d98dcf335bba6dad9c",
+    "url": "/React-Clicky-Game/static/js/main.50d7289f.chunk.js"
+  },
+  {
+    "revision": "c869cf8d0f6d585c0843",
+    "url": "/React-Clicky-Game/static/js/runtime-main.046d1f65.js"
+  },
+  {
+    "revision": "93778925e9b8ca4c78b569e738cc3827",
+    "url": "/React-Clicky-Game/static/media/background.93778925.jpeg"
+  }
+]);
